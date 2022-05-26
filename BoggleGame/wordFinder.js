3dac0a2953e77199021412cd4dbf849cf948b24b
Ben Davis <bdavis493@byui.edu>
2022-05-25 23:06:34 -0600

    Greatly optimized the recursion!

--- BoggleGame/wordFinder.js
+++ BoggleGame/wordFinder.js
@@ -1,37 +1,49 @@
-function traverse(words, word, block) {
-    word.push(block);
-    if (word.length >= 3) {
-      words.push(word);
-    }
+function traverse(words, word, block,shortenedWordList3,shortenedWordList5) {
+  word.push(block);
   
-    block.neighbors.forEach((neighbor) => {
-      if (!word.includes(neighbor) && word.length <= 8) {
-        traverse(
-          words,
-          word.map((x) => x),
-          neighbor
-        );
-      }
-    });
+  if (word.length == 3 && !shortenedWordList3.has(convertWordToString(word).toLowerCase())){
+    // console.log(shortenedWordList3.has(convertWordToString(word).toLowerCase()));
+    return;
   }
-  
+  else if (word.length == 5 && !shortenedWordList5.has(convertWordToString(word).toLowerCase())){
+    return
+  }
+  else if (word.length >= 3) {
+    words.push(word);
+  }
+
+  block.neighbors.forEach((neighbor) => {
+    if (!word.includes(neighbor) && word.length <= 10) {
+      traverse(
+        words,
+        word.map((x) => x),
+        neighbor,shortenedWordList3,
+        shortenedWordList5
+      );
+    }
+  });
+}
+
 function convertWordToString(w) {
   return w.map((block) => block.letter).join("");
 }
 
-const findAllWords = function (jsondata, grid,finalWords) {
+const findAllWords = function (jsondata, grid, finalWords) {
   let startTime = Date.now();
   let wordList = new Set(JSON.parse(jsondata));
-
+  let shortenedWordList3 = JSON.parse(jsondata).map(w => w.slice(0,3));
+  let shortenedWordList5 = JSON.parse(jsondata).map(w => w.slice(0,5));
+  shortenedWordList5 = new Set(shortenedWordList5);
+  shortenedWordList3 = new Set(shortenedWordList3);
+  console.log(shortenedWordList3);
   // wordList = wordList.filter(x => x.length<17)
 
   let words = [];
   let word = [];
 
-
   for (let i = 0; i < grid.length; i++) {
     word = [];
-    traverse(words, word, grid[i]);
+    traverse(words, word, grid[i],shortenedWordList3,shortenedWordList5);
     // outWords = outWords.concat(words);
   }
   // grid.forEach(block => {
@@ -60,6 +72,6 @@ const findAllWords = function (jsondata, grid,finalWords) {
 
   console.log(`time taken: ${Date.now() - startTime} milliseconds`);
   return finalWords;
-}
+};
 
 export default findAllWords;
